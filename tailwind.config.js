75730b542b4346a88653060a7e907a6242753bbf
Mathieu Soussignan <mathieu@ordinateur-portable-de-mathieu.home>
2024-09-29 16:23:55 +0200

    Update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        paradise: ['CallmeJigo', 'sans-serif'], // Ajoute cette ligne pour intégrer la nouvelle police
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
